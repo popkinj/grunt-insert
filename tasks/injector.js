4c77d6e13c7e2675f462f3787544122bc6928cde
Jamie Popkin <popkinj@littleearth.ca>
2013-12-12 13:46:34 -0800

    Inputs and outputs working.
    Reading both input and output files is working well. Next is to make a temporary backup.

--- tasks/injector.js
+++ tasks/injector.js
@@ -10,40 +10,47 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+	// Please see the Grunt documentation for more information regarding task
+	// creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('injector', 'Insert code from one file into another.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      removeComments: true
-    });
+	grunt.registerMultiTask('injector', 'Insert code from one file into another.', function() {
+		// Merge task-specific and/or target-specific options with these defaults.
+		var options = this.options({
+				removeComments: true
+		});
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+		// Iterate over all specified file groups.
+		this.files.forEach(function(f) {
+			// Read source file
+			var src = f.src.filter(function(filepath) {
+				// Warn on and remove invalid source files (if nonull was set).
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				} else {
+					return true;
+				}
+			}).map(function(filepath) {
+				return grunt.file.read(filepath); // Read file source.
+			});
 
-      // Handle options.
-      src += options.punctuation;
+			// Read destination file
+			if (!grunt.file.exists(f.dest)) {
+				grunt.log.warn('Destination file "' + f.dest + '" not found.');
+			} else {
+				var dest = grunt.file.read(f.dest); // Read file source.
+			}
+			grunt.log.warn(dest);
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+			// // Handle options.
+			// src += options.punctuation;
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+			// // Write the destination file.
+			// grunt.file.write(f.dest, src);
+
+			// // Print a success message.
+			// grunt.log.writeln('File "' + f.dest + '" created.');
+		});
+	});
 
 };
